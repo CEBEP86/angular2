e43b560d55f3c87ae2c6c45a0ee295e0c5fff8ef
CEBEP86 <blackmesa944@gmail.com>
2017-04-25 17:25:30 +0500

    routing on new version angular2 is work

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent }   from './about.component';
+import { HomeComponent }   from './home.component';
+import { NotFoundComponent }   from './not-found.component';
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent, HomeComponent, AboutComponent, NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
